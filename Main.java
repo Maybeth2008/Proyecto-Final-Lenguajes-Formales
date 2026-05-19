347186fdad68b819fb1fdc8a4fdfca58ddc79252
Maybeth2008 <lopezbedoya.2008@gmail.com>
2026-05-19 17:37:34 -0500

    Refactor variable input handling and analysis calls

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main{
 	public static void main(String[] args) {
 	    Scanner sc= new Scanner(System.in);
 	    
+	    
 		System.out.println("Ingrese el número de variables a definir");
 		int v= sc.nextInt();	    
 		String[] varia= new String[v];
@@ -14,21 +15,26 @@ public class Main{
 		String[] entrada= new String[n];
 		sc.nextLine(); // <- consumir el Enter pendiente
 
-		
-		
+		AnalisisEstatico analisis=new AnalisisEstatico();
+		ArrayList<AST> reglas = new ArrayList<>();
 		Interprete interprete= new Interprete();
 		
 		for(int j=0; j<v; j++){
 		    System.out.println("Ingrese la variable "+ (j+1));
 		    String vax=varia [j]=sc.nextLine();
-		    String[] partes = vax.split("=");
-		    String izquierda= partes[0].trim();
-		    String derecha= partes[1].trim();
-		    int nume= Integer.parseInt(derecha);
-		    interprete.agregarMap(izquierda,nume);
+		    
+		    if(vax.contains("=")){
+    		    String[] partes = vax.split("=");
+    		    String izquierda= partes[0].trim();
+    		    String derecha= partes[1].trim();
+    		    int nume= Integer.parseInt(derecha);
+    		    interprete.agregarMap(izquierda,nume);
+		    }else{
+		        interprete.hechos.add(vax.trim());
+		    }
+		    
 		}
 		
-
 		for(int i=0; i<n; i++){
 		    System.out.println("Ingrese la regla "+ (i+1));
 		    entrada [i]=sc.nextLine();
@@ -37,13 +43,18 @@ public class Main{
     		AnalisisLexico regla = new AnalisisLexico();
     		ArrayList<Token> tokens = regla.lexema(reglaActual);
     		AnalisisSintactico parser = new AnalisisSintactico(tokens);
-    		AST arbol=parser.Rule();    
+    		AST arbol=parser.Rule();        
+    		reglas.add(arbol);
     		interprete.evaluar(arbol);
 		}
 		
+		for(String hecho : interprete.hechos){
+            System.out.println(hecho);
+        }
+        analisis.redundancia(reglas);
+        analisis.conflictos(reglas);
+        analisis.inactivo(reglas);
         
-		
 	}
 
 }
-
